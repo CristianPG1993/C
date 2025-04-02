7218553e14130b1a590a6d53a094e81237ef1f9f
CristianPG1993 <cristian.panos.gallego@gmail.com>
2025-04-02 19:38:24 +0200

    Organización de ejercicios

---
+++ Array/Ejercicio4_invertir_array.cpp
@@ -0,0 +1,18 @@
+/*
+Ejercicio 4: Invertir un array usando punteros
+
+Descripción:
+Crea un programa en C++ que tome un array de enteros y lo invierta utilizando punteros. 
+No se permite usar índices (`array[i]`) dentro de la lógica de inversión: debes usar solo aritmética de punteros.
+
+Requisitos:
+1. Declarar un array de 5 números enteros.
+2. Usar punteros para invertir el orden de los elementos.
+3. Mostrar el array antes y después de invertirlo.
+4. Usar funciones para organizar el código.
+
+Objetivo:
+Practicar el uso de punteros para manipular datos en memoria, evitando el acceso tradicional por índices.
+*/
+
+#include <iostream>
